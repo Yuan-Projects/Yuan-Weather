ed9df29ecbab57fcdfd4e5c7cc06804d88ea60c6
rainyjune <rainyjune@live.cn>
2019-05-17 11:59:22 +0800

    Update index page

--- mp/pages/index/index.js
+++ mp/pages/index/index.js
@@ -1,23 +1,40 @@
 //index.js
 //获取应用实例
 const app = getApp();
-import { normalizeLocation } from '../../utils/util';
+import { normalizeLocation, normalizeDailyDate, getLevel } from '../../utils/util';
 import { getWeatherDataByLocation } from '../../utils/heweather';
 
 Page({
   data: {
-    city: ''
+    city: '',
+    currentWeatherImgSrc: '',
+    currentWeatherImgAlt: '',
+    currentTemp: '',
+    currentWeather: '',
+    observationTime: '',
+    temperatureRange: '',
+    airQualityLevel: '',
+    airQualityValue: '',
+    airQualityPM25Value: '',
+    forecast: []
   },
   onLoad: function () {
-    this.setData({
-      city: app.globalData.currentCity
-    });
     getWeatherDataByLocation('auto_ip', (data) => {
       const { location, current_observation, forecast, aqi } = data;
       const { icon_url, weather, temperature, observation_time, pm25 } = current_observation;
-
       this.setData({
-        city: normalizeLocation(location.state, location.city)
+        city: normalizeLocation(location.state, location.city),
+        currentTemp: `${temperature}${isNaN(temperature) ? '' : '℃'}`,
+        currentWeather: weather,
+        observationTime: observation_time,
+        temperatureRange: `${forecast[0].low_temperature} ~ ${forecast[0].high_temperature}℃`,
+        airQualityValue: current_observation.aqi,
+        airQualityLevel: `air_quality_${getLevel(current_observation.aqi)}`,
+        airQualityPM25Value: pm25,
+        forecast: forecast.map(item => {
+          item.date = normalizeDailyDate(item.date);
+          return item;
+        })
       })
     });
   }
