b59c713a72ff0a9197b429b9c17c0e5631b120b4
rainyjune <rainyjune@live.cn>
2020-11-18 18:39:07 +0800

    Update service worker

--- server/public/pwabuilder-sw.js
+++ server/public/pwabuilder-sw.js
@@ -1,47 +1,61 @@
-// This is the "Offline page" service worker
+const CACHE = "pages-cache-v1";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
+const filesToCache = [
+  '/',
+  'stylesheets/normalize.css',
+  'stylesheets/app.css',
+  'stylesheets/city.css',
+  'js/city.js',
+  'images/geolocation-icon.png',
+  'offline.html'
+];
 
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      .then(cache => cache.addAll(filesToCache))
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
+self.addEventListener('fetch', event => {
+  console.log('fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
       }
-    })());
-  }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request).then(response => {
+        // TODO 5 - Respond with custom 404 page
+        return caches.open(CACHE).then(cache => {
+          cache.put(event.request.url, response.clone());
+          return response;
+        });
+      });
+    })
+    .catch(error => {
+      console.log('caches error ', error);
+      return caches.match('offline.html');
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+
+  const cacheAllowlist = [CACHE];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
