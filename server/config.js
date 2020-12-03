bdb5eedfbc61865926b91c91c3a2fb3ef3cf6177
rainyjune <rainyjune@live.cn>
2020-12-03 14:12:19 +0800

    Remove wunderground support, add YIKEYUN support

--- server/config.js
+++ server/config.js
@@ -1,5 +1,9 @@
 module.exports = {
+  PROVIDER: 'heweather', // Supported: heweather | yikeyun
   API_SERVER_AUTH_KEY: '',
   GA_TRACKING_ID: '',
-  WUNDERGROUND_API: ''
+  YIKEYUN: {
+    appid: '',
+    appsecret: ''
+  }
 };
--- server/dataProvider/index.js
+++ server/dataProvider/index.js
@@ -1,11 +1,15 @@
 const axios = require('axios');
-const heweather = require('./heweather.js');
-const wunderground = require('./wunderground.js');
+const config = require('../config.js');
+let provider = null;
+if (config.PROVIDER === 'heweather') {
+  provider = require('./heweather.js')
+} else {
+  provider = require('./yikeyun.js');
+}
 
 function getWeatherDataByLocation(location) {
   return new Promise((resolve, reject) => {
-    heweather.getWeatherDataByLocation(location).then((res) => {
-      //console.log('ok 1', res)
+    provider.getWeatherDataByLocation(location).then((res) => {
       if (res.status === 'ok' && res.location.country_name === '中国') {
         resolve(res);
       } else {
@@ -13,13 +17,6 @@ function getWeatherDataByLocation(location) {
       }
     }).catch((err) => {
       console.log('erro 1', err)
-      wunderground.getWeatherDataByLocation(location).then((res) => {
-        console.log('ok 2')
-        resolve(res);
-      }).catch((e) => {
-        console.log('error 2', e)
-        reject(e);
-      });
     });
   });
 }
