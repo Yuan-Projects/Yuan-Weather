51bc894a0f2670ab14e6df14fb4c31fa4bd8ba94
rainyjune <rainyjune@live.cn>
2023-01-10 16:14:30 +0800

    Add search support

--- server/public/js/city.js
+++ server/public/js/city.js
@@ -1,48 +1,80 @@
 window.onload = function () {
-  document.getElementById('geolocButton').addEventListener('click', function(event) {
-    var classList = this.classList;
-    event.preventDefault();
-    if (classList.contains('locating')) {
-      return false;
+  document
+    .getElementById("geolocButton")
+    .addEventListener("click", function (event) {
+      var classList = this.classList;
+      event.preventDefault();
+      if (classList.contains("locating")) {
+        return false;
+      }
+
+      var startPos;
+      var geoOptions = {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 5000,
+      };
+
+      var nudge = document.getElementById("nudge");
+
+      var showNudgeBanner = function () {
+        nudge.style.display = "block";
+        setTimeout(hideNudgeBanner, 1500);
+      };
+
+      var hideNudgeBanner = function () {
+        nudge.style.display = "none";
+      };
+
+      var geoSuccess = function (position) {
+        hideNudgeBanner();
+        classList.remove("locating");
+        // Do magic with location
+        startPos = position;
+        fetch(
+          "/api/getcitybygeocode/" +
+            encodeURIComponent(
+              startPos.coords.latitude + "," + startPos.coords.longitude
+            )
+        )
+          .then((data) => {
+            return data.json();
+          })
+          .then((result) => {
+            if (result.city) {
+              window.location = "/city/" + encodeURIComponent(result.city);
+            }
+          });
+      };
+      var geoError = function (error) {
+        showNudgeBanner();
+        classList.remove("locating");
+      };
+
+      navigator.geolocation.getCurrentPosition(
+        geoSuccess,
+        geoError,
+        geoOptions
+      );
+      classList.add("locating");
+    });
+
+  // Search Form
+  const searchBtn = document.getElementById("searchButton");
+  const cityInputCtl = document.querySelector('input[name="cityKeyword"]');
+  searchBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (cityInputCtl.style.visibility === "visible") {
+      if (cityInputCtl.value) {
+        window.location.href = `/city/${encodeURIComponent(
+          cityInputCtl.value
+        )}`;
+      } else {
+        cityInputCtl.style.visibility = "hidden";
+      }
+    } else {
+      cityInputCtl.style.visibility = "visible";
     }
-
-    var startPos;
-    var geoOptions = {
-      enableHighAccuracy: true,
-      maximumAge: 0,
-      timeout: 5000
-    };
-
-    var nudge = document.getElementById("nudge");
-
-    var showNudgeBanner = function() {
-      nudge.style.display = "block";
-      setTimeout(hideNudgeBanner, 1500);
-    };
-
-    var hideNudgeBanner = function() {
-      nudge.style.display = "none";
-    };
-
-    var geoSuccess = function(position) {
-      hideNudgeBanner();
-      classList.remove('locating');
-      // Do magic with location
-      startPos = position;
-      fetch('/api/getcitybygeocode/' + encodeURIComponent(startPos.coords.latitude + ',' + startPos.coords.longitude)).then(data => {
-        return data.json()
-      }).then(result => {
-        if (result.city) {
-          window.location = '/city/' + encodeURIComponent(result.city);
-        }
-      })
-    };
-    var geoError = function(error) {
-      showNudgeBanner();
-      classList.remove('locating');
-    };
-
-    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
-    classList.add('locating');
   });
-}
+};
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -1,29 +1,29 @@
 // Cache Storage Name. Can be consider as the version of your web site.
-const CACHE = "pages-cache-v3";
+const CACHE = "pages-cache-v5";
 
 // Things that would make your site entirely non-functional if they failed to be fetched, things an equivalent native-app would make part of the initial download.
 // Ideal for: CSS, images, fonts, JS, templates… basically anything you'd consider static to that "version" of your site.
 const filesToCache = [
-  '/stylesheets/normalize.css',
-  '/stylesheets/app.css',
-  '/stylesheets/city.css',
-  '/js/city.js',
-  '/images/geolocation-icon.png',
-  '/offline.html'
+  "/stylesheets/normalize.css",
+  "/stylesheets/app.css",
+  "/stylesheets/city.css",
+  "/js/city.js",
+  "/images/geolocation-icon.png",
+  "/offline.html",
 ];
 
 // Frequently used files, but not as a dependency.
 // Ideal for: Resources that aren't needed straight away.
 const frequentlyUsedFiles = [
-  '/images/cond_icon/100.png', // Sunny
-  '/images/cond_icon/101.png', // Cloudy
+  "/images/cond_icon/100.png", // Sunny
+  "/images/cond_icon/101.png", // Cloudy
 ];
 
-self.addEventListener('install', event => {
-  console.log('installing')
+self.addEventListener("install", (event) => {
+  console.log("installing");
   event.waitUntil(
-    caches.open(CACHE).then(cache => {
-      // try to cache frequently used file, 
+    caches.open(CACHE).then((cache) => {
+      // try to cache frequently used file,
       // but they might not be cached if service worker finished handling events.
       cache.addAll(frequentlyUsedFiles);
       // cache dependecy files, if any of the resources fail to be fetched, the `cache.addAll()` call rejects.
@@ -32,25 +32,27 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
-  console.log('fetch event for ', event.request.url);
+self.addEventListener("fetch", (event) => {
+  console.log("fetch event for ", event.request.url);
   // Parse the URL:
   var requestURL = new URL(event.request.url);
-  console.log('requestURL:', requestURL);
+  console.log("requestURL:", requestURL);
   // Routing for local URLs
   if (requestURL.origin === location.origin) {
     // Network first and save to cache, always show the latest content for the index/weather page.
     // Read from cache if online.
-    if (requestURL.pathname === '/' || /^\/city\//.test(requestURL.pathname)) {
+    if (requestURL.pathname === "/" || /^\/city\//.test(requestURL.pathname)) {
       event.respondWith(
-        fetch(event.request).then(response => {
-          return caches.open(CACHE).then(cache => {
-            cache.put(event.request.url, response.clone());
-            return response;
-          });
-        }).catch(function () {
-          return caches.match(event.request);
-        }),
+        fetch(event.request)
+          .then((response) => {
+            return caches.open(CACHE).then((cache) => {
+              cache.put(event.request.url, response.clone());
+              return response;
+            });
+          })
+          .catch(function () {
+            return caches.match(event.request);
+          })
       );
       return;
     }
@@ -58,46 +60,47 @@ self.addEventListener('fetch', event => {
 
   // A sensible default pattern
   event.respondWith(
-    caches.match(event.request)
-    .then(response => {
-      if (response) {
-        //console.log('Found ', event.request.url, ' in cache');
-        return response;
-      }
-      //console.log('Network request for ', event.request.url);
-      return fetch(event.request).then(response => {
-        console.log('response:', response);
-        if (response.type !== 'basic') {
+    caches
+      .match(event.request)
+      .then((response) => {
+        if (response) {
+          //console.log('Found ', event.request.url, ' in cache');
           return response;
         }
-        // TODO 5 - Respond with custom 404 page
-        return caches.open(CACHE).then(cache => {
-          cache.put(event.request.url, response.clone());
-          return response;
+        //console.log('Network request for ', event.request.url);
+        return fetch(event.request).then((response) => {
+          console.log("response:", response);
+          if (response.type !== "basic") {
+            return response;
+          }
+          // TODO 5 - Respond with custom 404 page
+          return caches.open(CACHE).then((cache) => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
         });
-      });
-    })
-    .catch(error => {
-      console.log('caches error ', error);
-      return caches.match('offline.html');
-    })
+      })
+      .catch((error) => {
+        console.log("caches error ", error);
+        return caches.match("offline.html");
+      })
   );
 });
 
-self.addEventListener('activate', event => {
-  console.log('Activating new service worker...');
+self.addEventListener("activate", (event) => {
+  console.log("Activating new service worker...");
 
   const cacheAllowlist = [CACHE];
 
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames
           // Return true if you want to remove this cache,
           // but remember that caches are shared across the whole origin
-          .filter(cacheName => cacheAllowlist.indexOf(cacheName) === -1)
+          .filter((cacheName) => cacheAllowlist.indexOf(cacheName) === -1)
           // Delete unused caches.
-          .map(cacheName => caches.delete(cacheName))
+          .map((cacheName) => caches.delete(cacheName))
       );
     })
   );
